25f328a805cf318e47c42bafef38dbb2b4077f84
Mostafa ElBadawy <mostafa.elbadawy@fixedmea.com>
2025-05-14 17:55:01 +0300

    Initial project setup with Xcode

---
+++ MoviesBox/MoviesBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBox
+//
+//  Created by Mostafa ElBadawy on 14/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
